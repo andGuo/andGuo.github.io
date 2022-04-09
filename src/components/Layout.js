e925f1a9380bf214db07c6acbf23aa6b79d752ba
Andrew Guo <andrew.c.guo@gmail.com>
2022-04-09 18:40:42 -0400

    lots of flexboxes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,14 +3,16 @@ import NavBar from './NavBar'
 
 export default function Layout({ children }) {
     return (
-        <div className="layout bg-secondaryDark text-primaryDark">
+        <div className="layout min-h-screen bg-gradient-to-br from-secondaryDark to-gray-700 text-primaryDark">
             <NavBar />
-            <div className="content">
-                {children}
+            <div className="flex flex-col flex-grow justify-between">
+                <div className="content">
+                    {children}
+                </div>
+                <footer className="flex justify-center align-middle content-start">
+                    <p className="p-8">andGuo 2022</p>
+                </footer>
             </div>
-            <footer>
-                <p>andGuo 2022</p>
-            </footer>
         </div>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,12 +7,17 @@ export default function NavBar() {
             <div className="logo h-12 px-4 py-1 text-5xl">
                 <Link className="hover:text-gray-300" to="/">My Site</Link>
             </div>
-            <div>
-                <ul className="flex menu mt-2.5 p-3 text-xl ">
+            <div className="hidden sm:flex">
+                <ul className="flex menu mt-2.5 p-3 text-xl">
                     <li className="item mx-3 px-1.5 hover:text-gray-300"><Link to="/about">About me</Link></li>
                     <li className="item mx-3 px-1.5 hover:text-gray-300"><Link to="/portfolio">Portfolio</Link></li>
                     <li className="item mx-3 px-1.5 hover:text-gray-300"><Link to="/wasmProject">WASM Thing</Link></li>
                 </ul>
+            </div>
+            <div className="sm:hidden flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mx-3 px-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
             </div>
         </nav>
     )
