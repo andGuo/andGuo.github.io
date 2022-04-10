d8a8c81ddc7b94eb4bc6fff7ab6fd60f54aedba2
Andrew Guo <andrew.c.guo@gmail.com>
2022-04-10 02:59:21 -0400

    SEO stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,15 @@
 module.exports = {
   siteMetadata: {
     title: `Andrew Guo`,
+    author: '@andGuo',
     description:
-      "Personal website for Andrew Guo, student at Carleton University",
+      "Personal website for Andrew Guo",
     siteUrl: `https://andguo.github.io/`,
+    keywords: 'Ottawa, Canada, Carleton, CS, resume, personal website, react, gatsby, tailwind, guo, andrew, student'
   },
   plugins: [
     'gatsby-plugin-postcss',
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
---
+++ src/components/Seo.js
@@ -0,0 +1,75 @@
+import * as React from 'react'
+import { Helmet } from 'react-helmet';
+
+function SEO({ description, lang, meta, title}) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+            keywords
+          }
+        }
+      }
+    `
+  )
+}
+
+const keywords = site.siteMetadata.keywords
+const metaDescription = description || site.siteMetadata.description
+const defaultTitle = site.siteMetadata?.title
+
+const Header = ( { title }) => {
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          name: `keywords`,
+          content: keywords,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:creator`,
+          content: site.siteMetadata?.author || ``,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+      ].concat(meta)}
+    />
+  )
+}
+
+export default Header;
