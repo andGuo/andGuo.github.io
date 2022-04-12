f19165092abdb57d26e9a45713718e10a48da910
Andrew Guo <andrew.c.guo@gmail.com>
2022-04-12 03:54:53 -0400

    debugging analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,17 +37,17 @@ module.exports = {
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
-        gtagConfig: {
+        // gtagConfig: {
           // optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
+          // anonymize_ip: true,
+          // cookie_expires: 0,
+        // },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
           head: true,
           // Setting this parameter is also optional
-          respectDNT: true,
+          // respectDNT: true,
           // Avoids sending pageview hits from custom paths
           // exclude: ["/preview/**", "/do-not-track/me/too/"],
           // Defaults to https://www.googletagmanager.com
