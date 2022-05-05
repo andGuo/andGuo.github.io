97d67df67b7d39a4b9e24efbdce190bcd9aaa3d2
Andrew Guo <andrew.c.guo@gmail.com>
2022-05-04 22:13:46 -0400

    enabled respect DNT

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
           // Puts tracking script in the head instead of the body
           head: true,
           // Setting this parameter is also optional
-          // respectDNT: true,
+          respectDNT: true,
           // Avoids sending pageview hits from custom paths
           // exclude: ["/preview/**", "/do-not-track/me/too/"],
           // Defaults to https://www.googletagmanager.com
